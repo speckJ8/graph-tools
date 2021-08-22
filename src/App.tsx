fddcace8046e073a7c8f8e8a0b758563758f7d21
clx <junioralcides1994@gmail.com>
2021-08-23 08:33:29 +0900

    style MetadataArea

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,22 @@ import ResizableContainer from "./components/ResizableContainer"
 import TextEditorArea from './components/TextEditorArea'
 import MetadataArea from './components/MetadataArea'
 import DisplayArea from './components/DisplayArea'
-import FooterArea from "./components/FooterArea"
 
 
 export default class App extends React.Component {
     render () {
         return (
-        <div className="app-container flex flex-col">
-            <div className="flex-1 flex w-full h-full">
-                <div className="w-52 flex border-r">
-                    <MetadataArea />
-                </div>
-                <div className="flex flex-grow">
-                    <DisplayArea />
-                </div>
-                <ResizableContainer style={{ width: "500px" }}
-                        className="border-l">
-                    <TextEditorArea/>
-                </ResizableContainer>
+        <div className="app-container flex">
+            <div className="w-60">
+                <MetadataArea />
             </div>
-            <FooterArea statusMessage="All systems reloaded"/>
+            <div className="flex-grow">
+                <DisplayArea />
+            </div>
+            <ResizableContainer style={{ width: "600px" }}
+                    className="border-l">
+                <TextEditorArea/>
+            </ResizableContainer>
         </div>
         )
     }
