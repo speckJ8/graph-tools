a5e1b95be3ea6bf199f062e5b42f8a4f25ab9fd2
Alcides Andrade <junioralcides1994@gmail.com>
2022-01-05 13:39:00 +0900

    loading circle style

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,17 @@ module.exports = {
             blue: colors.blue,
             red: colors.rose,
             yellow: colors.amber,
+        },
+        boxShadow: {
+            sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+            DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+            md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+            lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+            xl: '0 0px 25px -5px rgba(0, 0, 0, 0.1), 0 0px 25px -5px rgba(0, 0, 0, 0.04)',
         }
+    },
+    variants: {
+        fill: ['hover', 'focus'],
+        stroke: ['hover', 'focus'],
     }
 }
